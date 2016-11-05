e80aa032aa5b5bac9513183e4d77717654d7618f
jx520 <jerryjin0630@qq.com>
2016-11-05 17:51:41 +0800

    还在研究这东西怎么玩。原来CSDN的CODE也是作为客户端编辑。也要进行git操作才会把东西推到code里去。 git commit -m 还在研究这东西怎么玩。原来CSDN的CODE也是作为客户端编辑。也要进行git操作才会把东西推到code里去。 git commit -m 还在研究这东西怎么玩。原来CSDN的CODE也是作为客户端编辑。也要进行git操作才会把东西推到code里去。

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    echo "hehe,哈哈！";
+?>
+<?php
+	// $mysqli = new mysqli("localhost:3306","root","root12345");
+	// if($mysqli->connect_error) echo "Mysql Connect Error!";
+	// else echo "mysql 连接成功！";
+	// $mysqli->close();
+	// $j = 1;
+?>
